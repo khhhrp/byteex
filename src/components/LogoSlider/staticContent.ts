4965ba4576bad3e87391a6d2df821cc5b94b9f63
vdmkhrp <vdmkhrp@gmail.com>
2026-01-26 21:57:00 +0200

    fix images

--- src/components/LogoSlider/staticContent.ts
+++ src/components/LogoSlider/staticContent.ts
@@ -1,8 +1,8 @@
-import artBoard01 from "../../assets/images/artBoard01.webp";
-import artBoard02 from "../../assets/images/artBoard02.webp";
-import artBoard03 from "../../assets/images/artBoard03.webp";
-import artBoard04 from "../../assets/images/artBoard04.webp";
-import artBoard05 from "../../assets/images/artBoard05.webp";
+import artBoard01 from "../../assets/images/artboard01.webp";
+import artBoard02 from "../../assets/images/artboard02.webp";
+import artBoard03 from "../../assets/images/artboard03.webp";
+import artBoard04 from "../../assets/images/artboard04.webp";
+import artBoard05 from "../../assets/images/artboard05.webp";
 
 export const logosMobile = [
   artBoard01,
